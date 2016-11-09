5c2e0179102c088b122361d614b464c9cf6418c9
Flerry <nuryelse@gmail.com>
2016-11-09 09:34:01 +0300

    first version

---
+++ src/ParseDocs.java
@@ -0,0 +1,44 @@
+package com.flerry.tgbot.javadocs;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class ParseDocs {
+	private static String goodlink;
+
+	private static void linkParse(String requestWord) throws IOException {
+		String url = "https://search.oracle.com/search/search?search_p_main_operator=all&group=Documentation&q="
+				+ requestWord
+				+ "+url%3A%2Fjavase%2F8%2Fdocs%2Fapi&searchField="
+				+ requestWord + "&docsets=%2Fjavase%2F8%2Fdocs%2Fapi";
+		String allLink;
+		ArrayList<String> links = new ArrayList<String>();
+
+		Document doc = Jsoup.connect(url).get();
+		Elements aElements = doc.select("a[href]");
+		for (Element link : aElements) {
+			allLink = link.attr("abs:href");
+			if (!allLink.contains("search") && allLink.contains(requestWord)) {
+				links.add(link.attr("abs:href"));
+
+			}
+
+		}
+		goodlink = links.get(0);
+
+	}
+
+	public static String docsTextLink(String requestWord) throws IOException {
+
+		ParseDocs.linkParse(requestWord);
+		Document doc2 = Jsoup.connect(goodlink).get();
+		String tElements = doc2.select("div[class=block]").first().text();
+		return "\nСсылка на документацию:" + " " + goodlink + "\n\n"
+				+ "Краткое описание:" + "\n" + tElements;
+	}
+}
---
+++ src/TgBot.java
@@ -0,0 +1,91 @@
+package com.flerry.tgbot.javadocs;
+
+import org.telegram.telegrambots.TelegramApiException;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+
+import java.io.IOException;
+
+public class TgBot extends TelegramLongPollingBot {
+	public static void main(String[] args) {
+		TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+		try {
+			telegramBotsApi.registerBot(new TgBot());
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String getBotUsername() {
+		return "JavaDocBot";
+	}
+
+	public String getBotToken() {
+		return "297793566:AAFL_MphiG7gDin6CP97lTPR8_Z4Y_r3soA";
+	}
+
+	public void onUpdateReceived(Update update) {
+
+		Message message = update.getMessage();
+		if (message != null && message.hasText()) {
+			if (message.getText().contains("/help")
+					|| message.getText().equals("/start")
+					|| message.getText().equals("/help")) {
+				sendMsg(message,
+						"Привет, я JavaDocBot. Я всегда готов помочь тебе, о Java-кодер, в поиске документации :) Пожалуйста, напиши нужный класс/метод/исключение для поиска нужной документации");
+
+			}
+			if (message.getText().contains("@JavaDocsBot")) {
+				try {
+					sendMsg(message,
+							ParseDocs.docsTextLink(message.getText()
+									.replace("@JavaDocsBot ", "")
+									.substring(0, 1).toUpperCase()
+									+ message.getText()
+											.replace("@JavaDocsBot ", "")
+											.substring(1)));
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (IndexOutOfBoundsException a) {
+					sendMsg(message,
+							"Извини, но я не смог найти информации по твоему запросу");
+				}
+			}
+			if (message.getChatId() < 0) {
+				System.out.println("Сообщение из группового чата");
+			} else if (!message.getText().contains("/help")
+					&& !message.getText().contains("/start")
+					&& !message.getText().contains("@JavaDocsBot")) {
+				try {
+					sendMsg(message,
+							(ParseDocs.docsTextLink(message.getText()
+									.substring(0, 1).toUpperCase()
+									+ message.getText().substring(1))));
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (IndexOutOfBoundsException a) {
+					sendMsg(message,
+							"Извини, но я не смог найти информации по твоему запросу");
+				}
+
+			}
+
+		}
+	}
+
+	private void sendMsg(Message message, String text) {
+		SendMessage sendMessage = new SendMessage();
+		sendMessage.enableMarkdown(true);
+		sendMessage.setChatId(message.getChatId().toString());
+		sendMessage.setReplyToMessageId(message.getMessageId());
+		sendMessage.setText(text);
+		try {
+			sendMessage(sendMessage);
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+		}
+	}
+}
