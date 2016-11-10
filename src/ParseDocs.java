bce6aedc7cc077757eded3d2c33f2d6634a58401
Flerry <nuryelse@gmail.com>
2016-11-10 14:55:38 +0300

    Add constant string and edit URL architecture

--- src/ParseDocs.java
+++ src/ParseDocs.java
@@ -1,45 +1,61 @@
 package com.flerry.tgbot.javadocs;
 
-import java.io.IOException;
-import java.util.ArrayList;
-
+import org.apache.http.client.utils.URIBuilder;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+
 public class ParseDocs {
-	private static String goodlink;
+	private static final String REQUEST_EXCEPTION = "Запрос пуст или содержит запрещенные символы!";
+	private static String goodLink;
+	private static URI url;
 
 	private static void linkParse(String requestWord) throws IOException {
-		String url = "https://search.oracle.com/search/search?search_p_main_operator=all&group=Documentation&q="
-				+ requestWord
-				+ "+url%3A%2Fjavase%2F8%2Fdocs%2Fapi&searchField="
-				+ requestWord + "&docsets=%2Fjavase%2F8%2Fdocs%2Fapi";
-		String allLink;
-		ArrayList<String> links = new ArrayList<String>();
+		if (requestWord.length() == 0 || requestWord.contains("\'")) {
+			System.out.println(REQUEST_EXCEPTION);
 
-		Document doc = Jsoup.connect(url).timeout(20000).get();
+		} else {
+			
+			try{
+			 url = new URIBuilder("https://search.oracle.com/search/search?search_p_main_operator=all&group=Documentation&")		
+		    .addParameter("q", requestWord)
+		    .addParameter("", "+url%3A%2Fjavase%2F8%2Fdocs%2Fapi&search")
+		    .addParameter("Field", requestWord)
+		    .addParameter("","&docsets=%2Fjavase%2F8%2Fdocs%2Fapi" )
+		    .build();
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+			String allLink;
+			ArrayList<String> links = new ArrayList<String>();
 
-		Elements aElements = doc.select("a[href]");
-		for (Element link : aElements) {
-			allLink = link.attr("abs:href");
-			if (!allLink.contains("search") && allLink.contains(requestWord)) {
-				links.add(link.attr("abs:href"));
+			Document doc = Jsoup.connect(url.toString()).timeout(20000).get();
+			Elements aElements = doc.select("a[href]");
+			for (Element link : aElements) {
+				allLink = link.attr("abs:href");
+				if (!allLink.contains("search")
+						&& allLink.contains(requestWord)) {
+					links.add(link.attr("abs:href"));
+
+				}
 
 			}
 
+			goodLink = links.get(0);
 		}
-		goodlink = links.get(0);
-
 	}
 
 	public static String docsTextLink(String requestWord) throws IOException {
 
 		ParseDocs.linkParse(requestWord);
-		Document doc2 = Jsoup.connect(goodlink).timeout(20000).get();
+		Document doc2 = Jsoup.connect(goodLink).timeout(20000).get();
 		String tElements = doc2.select("div[class=block]").first().text();
-		return "\nСсылка на документацию:" + " " + goodlink + "\n\n"
+		return "\nСсылка на документацию:" + " " + goodLink + "\n\n"
 				+ "Краткое описание:" + "\n" + tElements;
 	}
 }
--- src/TgBot.java
+++ src/TgBot.java
@@ -10,6 +10,17 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import java.io.IOException;
 
 public class TgBot extends TelegramLongPollingBot {
+	private static final String COMMAND_HELP = "/help";
+	private static final String COMMAND_START = "/start";
+	private static final String COMMAND_HELP_BOT = "/help@JavaDocsBot";
+	private static final String COMMAND_START_BOT = "/start@JavaDocsBot";
+	private static final String COMMAND_APPEAL = "@JavaDocsBot";
+	private static final String COMMAND_APPEAL_WITH_SPACE = "@JavaDocsBot ";
+	private static final String MESSAGE_HELP = "Привет, я JavaDocsBot. Я всегда готов помочь тебе, о Java-кодер, в поиске документации :) Пожалуйста, напиши нужный класс/метод/исключение для поиска нужной документации";
+	private static final String MESSAGE_NON_RESULT = "Извини, но я не смог найти информации по твоему запросу";
+	private static final String MESSAGE_GET_RESULT_TIME_OUT = "Ошибка поиска, попробуй позже";
+	private static final String MESSAGE_FROM_GROUP_CHAT = "Сообщение из группового чата";
+
 	public static void main(String[] args) {
 		TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
 		try {
@@ -20,62 +31,57 @@ public class TgBot extends TelegramLongPollingBot {
 	}
 
 	public String getBotUsername() {
-<<<<<<< HEAD
+
 		return "JavaDocsBot";
-=======
-		return "your_bot_name";
->>>>>>> 131cd9bb14032d2aa8923da8304a2869f985461c
 	}
 
 	public String getBotToken() {
-		return "your_token";
+		return "297793566:AAFL_MphiG7gDin6CP97lTPR8_Z4Y_r3soA";
 	}
 
 	public void onUpdateReceived(Update update) {
 
 		Message message = update.getMessage();
 		if (message != null && message.hasText()) {
-			if (message.getText().equals("/start")
-					|| message.getText().equals("/help")
-					|| message.getText().equals("/help@JavaDocsBot")
-					|| message.getText().equals("/start@JavaDocsBot")) {
-				sendMsg(message,
-						"Привет, я JavaDocBot. Я всегда готов помочь тебе, о Java-кодер, в поиске документации :) Пожалуйста, напиши нужный класс/метод/исключение для поиска нужной документации");
+			if (message.getText().equals(COMMAND_START_BOT)
+					|| message.getText().equals(COMMAND_HELP_BOT)
+					|| message.getText().equals(COMMAND_START)
+					|| message.getText().equals(COMMAND_HELP)) {
+				sendMsg(message, MESSAGE_HELP);
 
 			}
-			if (message.getText().contains("@JavaDocsBot")
-					&& !message.getText().equals("/help@JavaDocsBot")
-					&& !message.getText().equals("/start@JavaDocsBot")) {
+			if (message.getText().contains(COMMAND_APPEAL)
+					&& !message.getText().equals(COMMAND_HELP_BOT)
+					&& !message.getText().equals(COMMAND_START_BOT)) {
 				try {
 					sendMsg(message,
 							ParseDocs.docsTextLink(message.getText()
-									.replace("@JavaDocsBot ", "")
+									.replace(COMMAND_APPEAL_WITH_SPACE, "")
 									.substring(0, 1).toUpperCase()
-									+ message.getText()
-											.replace("@JavaDocsBot ", "")
-											.substring(1)));
+									+ message
+											.getText()
+											.replace(COMMAND_APPEAL_WITH_SPACE,
+													"").substring(1)));
 				} catch (IOException e) {
-					sendMsg(message, "Ошибка поиска, попробуй позже");
+					sendMsg(message, MESSAGE_GET_RESULT_TIME_OUT);
 				} catch (IndexOutOfBoundsException a) {
-					sendMsg(message,
-							"Извини, но я не смог найти информации по твоему запросу");
+					sendMsg(message, MESSAGE_NON_RESULT);
 				}
 			}
 			if (message.getChatId() < 0) {
-				System.out.println("Сообщение из группового чата");
-			} else if (!message.getText().contains("/help")
-					&& !message.getText().contains("/start")
-					&& !message.getText().contains("@JavaDocsBot")) {
+				System.out.println(MESSAGE_FROM_GROUP_CHAT);
+			} else if (!message.getText().contains(COMMAND_HELP)
+					&& !message.getText().contains(COMMAND_START)
+					&& !message.getText().contains(COMMAND_APPEAL)) {
 				try {
 					sendMsg(message,
 							(ParseDocs.docsTextLink(message.getText()
 									.substring(0, 1).toUpperCase()
 									+ message.getText().substring(1))));
 				} catch (IOException e) {
-					sendMsg(message, "Ошибка поиска, попробуй позже");
+					sendMsg(message, MESSAGE_GET_RESULT_TIME_OUT);
 				} catch (IndexOutOfBoundsException a) {
-					sendMsg(message,
-							"Извини, но я не смог найти информации по твоему запросу");
+					sendMsg(message, MESSAGE_NON_RESULT);
 				}
 
 			}
