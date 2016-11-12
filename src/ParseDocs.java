8a2ce4efc9c5381d123ae8c720a7e461a9901067
Flerry <nuryelse@gmail.com>
2016-11-12 11:43:32 +0300

    Added search methods in Java classes

--- src/ParseDocs.java
+++ src/ParseDocs.java
@@ -10,52 +10,75 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
 
-public class ParseDocs {
-	private static final String REQUEST_EXCEPTION = "Запрос пуст или содержит запрещенные символы!";
-	private static String goodLink;
-	private static URI url;
+ class ParseDocs {
+    private static final String REQUEST_EXCEPTION = "Запрос пуст или содержит запрещенные символы!";
+    private static String goodLinkClass;
+    private static URI url;
 
-	private static void linkParse(String requestWord) throws IOException {
-		if (requestWord.length() == 0 || requestWord.contains("\'")) {
-			System.out.println(REQUEST_EXCEPTION);
+    private static void linkClassParse(String requestWord) throws IOException {
+        if (requestWord.length() == 0 || requestWord.contains("\'")) {
+            System.out.println(REQUEST_EXCEPTION);
 
-		} else {
-			
-			try{
-			 url = new URIBuilder("https://search.oracle.com/search/search?search_p_main_operator=all&group=Documentation&")		
-		    .addParameter("q", requestWord)
-		    .addParameter("", "+url%3A%2Fjavase%2F8%2Fdocs%2Fapi&search")
-		    .addParameter("Field", requestWord)
-		    .addParameter("","&docsets=%2Fjavase%2F8%2Fdocs%2Fapi" )
-		    .build();
-			}catch(Exception e){
-				e.printStackTrace();
-			}
-			String allLink;
-			ArrayList<String> links = new ArrayList<String>();
+        } else {
 
-			Document doc = Jsoup.connect(url.toString()).timeout(20000).get();
-			Elements aElements = doc.select("a[href]");
-			for (Element link : aElements) {
-				allLink = link.attr("abs:href");
-				if (!allLink.contains("search")
-						&& allLink.contains(requestWord)) {
-					links.add(link.attr("abs:href"));
+            try {
+                url = new URIBuilder("https://search.oracle.com/search/search?search_p_main_operator=all&group=Documentation&")
+                        .addParameter("q", requestWord)
+                        .addParameter("", "+url%3A%2Fjavase%2F8%2Fdocs%2Fapi&search")
+                        .addParameter("Field", requestWord)
+                        .addParameter("", "&docsets=%2Fjavase%2F8%2Fdocs%2Fapi")
+                        .build();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            String allLink;
+            ArrayList<String> links = new ArrayList<String>();
 
-				}
+            Document doc = Jsoup.connect(url.toString()).timeout(20000).get();
+            Elements aElements = doc.select("a[href]");
+            for (Element link : aElements) {
+                allLink = link.attr("abs:href");
+                if (!allLink.contains("search")
+                        && allLink.contains(requestWord)) {
+                    links.add(link.attr("abs:href"));
 
-			}
+                }
 
-			goodLink = links.get(0);
-		}
-	}
+            }
 
-	public static String docsTextLink(String requestWord) throws IOException {
+            goodLinkClass = links.get(0);
 
-		ParseDocs.linkParse(requestWord);
-		Document doc2 = Jsoup.connect(goodLink).timeout(20000).get();
-		String tElements = doc2.select("div[class=block]").first().text();
-		return "\nСсылка на документацию:" + " " + goodLink + "\n\n"
-				+ "Краткое описание:" + "\n" + tElements;
-	}
+        }
+    }
+
+    public static String docsClassTextLink(String requestClass) throws IOException {
+
+        ParseDocs.linkClassParse(requestClass);
+        Document doc = Jsoup.connect(goodLinkClass).timeout(20000).get();
+        String tElements = doc.select("div[class=block]").first().text();
+        if (tElements.length() > 3500) {
+            tElements = doc.select("div[class=block]").first().text().substring(1, 3500) + "...";
+        }
+        return "\nСсылка на документацию:" + " " + goodLinkClass + "\n\n"
+                + "Краткое описание:" + "\n" + tElements;
+    }
+
+    public static String docsMethodTextLink(String requestClass, String requestMethod) throws IOException {
+        String goodLinkMethod;
+        ParseDocs.linkClassParse(requestClass);
+        Document doc = Jsoup.connect(goodLinkClass).timeout(20000).get();
+        Elements aElements = doc.select("a[href]");
+        String allLink;
+        ArrayList<String> links = new ArrayList<String>();
+        for (Element link : aElements) {
+            allLink = link.attr("abs:href");
+            if (allLink.contains(requestMethod)) {
+                links.add(link.attr("abs:href"));
+
+
+            }
+        }
+        goodLinkMethod = links.get(0);
+        return "\nСсылка на документацию:" + " " + goodLinkMethod;
+    }
 }
--- src/TgBot.java
+++ src/TgBot.java
@@ -16,7 +16,7 @@ public class TgBot extends TelegramLongPollingBot {
 	private static final String COMMAND_START_BOT = "/start@JavaDocsBot";
 	private static final String COMMAND_APPEAL = "@JavaDocsBot";
 	private static final String COMMAND_APPEAL_WITH_SPACE = "@JavaDocsBot ";
-	private static final String MESSAGE_HELP = "Привет, я JavaDocsBot. Я всегда готов помочь тебе, о Java-кодер, в поиске документации :) Пожалуйста, напиши нужный класс/метод/исключение для поиска нужной документации";
+	private static final String MESSAGE_HELP = "Привет, я JavaDocsBot. Я всегда готов помочь тебе, о Java-кодер, в поиске документации :) Пожалуйста, напиши нужный класс/метод/исключение для поиска нужной документации. Обрати внимание! Если ты ищешь метод, запрос должен выглядеть так: Class-method";
 	private static final String MESSAGE_NON_RESULT = "Извини, но я не смог найти информации по твоему запросу";
 	private static final String MESSAGE_GET_RESULT_TIME_OUT = "Ошибка поиска, попробуй позже";
 	private static final String MESSAGE_FROM_GROUP_CHAT = "Сообщение из группового чата";
@@ -50,40 +50,83 @@ public class TgBot extends TelegramLongPollingBot {
 				sendMsg(message, MESSAGE_HELP);
 
 			}
-			if (message.getText().contains(COMMAND_APPEAL)
-					&& !message.getText().equals(COMMAND_HELP_BOT)
-					&& !message.getText().equals(COMMAND_START_BOT)) {
-				try {
-					sendMsg(message,
-							ParseDocs.docsTextLink(message.getText()
-									.replace(COMMAND_APPEAL_WITH_SPACE, "")
-									.substring(0, 1).toUpperCase()
-									+ message
-											.getText()
-											.replace(COMMAND_APPEAL_WITH_SPACE,
-													"").substring(1)));
-				} catch (IOException e) {
-					sendMsg(message, MESSAGE_GET_RESULT_TIME_OUT);
-				} catch (IndexOutOfBoundsException a) {
-					sendMsg(message, MESSAGE_NON_RESULT);
-				}
-			}
-			if (message.getChatId() < 0) {
-				System.out.println(MESSAGE_FROM_GROUP_CHAT);
-			} else if (!message.getText().contains(COMMAND_HELP)
-					&& !message.getText().contains(COMMAND_START)
-					&& !message.getText().contains(COMMAND_APPEAL)) {
-				try {
-					sendMsg(message,
-							(ParseDocs.docsTextLink(message.getText()
-									.substring(0, 1).toUpperCase()
-									+ message.getText().substring(1))));
-				} catch (IOException e) {
-					sendMsg(message, MESSAGE_GET_RESULT_TIME_OUT);
-				} catch (IndexOutOfBoundsException a) {
-					sendMsg(message, MESSAGE_NON_RESULT);
-				}
+			try {
+				if (message.getText().contains(COMMAND_APPEAL)
+						&& !message.getText().contains("-")
+						&& !message.getText().equals(COMMAND_HELP_BOT)
+						&& !message.getText().equals(COMMAND_START_BOT)) {
 
+					String editMsg = message.getText()
+							.replace(COMMAND_APPEAL_WITH_SPACE, "")
+							.substring(0, 1).toUpperCase()
+							+ message.getText()
+									.replace(COMMAND_APPEAL_WITH_SPACE, "")
+									.substring(1);
+
+					sendMsg(message, ParseDocs.docsClassTextLink(editMsg));
+
+				}
+				if (message.getChatId() < 0
+						&& !message.getText().contains(COMMAND_APPEAL)) {
+					System.out.println(MESSAGE_FROM_GROUP_CHAT);
+				} else if (!message.getText().contains(COMMAND_HELP)
+						&& !message.getText().contains(COMMAND_START)
+						&& !message.getText().contains(COMMAND_APPEAL)
+						&& !message.getText().contains("-")) {
+
+					String editMsg = message.getText().substring(0, 1)
+							.toUpperCase()
+							+ message.getText().substring(1);
+
+					sendMsg(message, ParseDocs.docsClassTextLink(editMsg));
+
+				} else if (message.getText().contains(COMMAND_APPEAL)
+						&& message.getText().contains("-")
+						&& !message.getText().equals(COMMAND_HELP_BOT)
+						&& !message.getText().equals(COMMAND_START_BOT)) {
+
+					String editRow = message.getText().replace(
+							COMMAND_APPEAL_WITH_SPACE, "");
+
+					String editMsg = ParseDocs
+							.docsMethodTextLink((editRow.substring(0, 1)
+									.toUpperCase() + editRow.substring(1,
+									editRow.indexOf("-"))), (((editRow
+									.substring(editRow.indexOf("-") + 1,
+											(editRow.indexOf("-") + 2)))
+									.toLowerCase() + editRow.substring(editRow
+									.indexOf("-") + 2))));
+
+					sendMsg(message, editMsg);
+
+				} else if (message.getText().contains("-")
+						&& !message.getText().contains(COMMAND_APPEAL)) {
+
+					String editMsg = ParseDocs
+							.docsMethodTextLink(
+									(message.getText().substring(0, 1)
+											.toUpperCase() + message.getText()
+											.substring(
+													1,
+													message.getText().indexOf(
+															"-"))),
+									(((message.getText()
+											.substring(
+													(message.getText().indexOf(
+															"-") + 1),
+													(message.getText().indexOf(
+															"-") + 2)))
+											.toLowerCase() + message.getText()
+											.substring(
+													message.getText().indexOf(
+															"-") + 2))));
+
+					sendMsg(message, editMsg);
+				}
+			} catch (IOException e) {
+				sendMsg(message, MESSAGE_GET_RESULT_TIME_OUT);
+			} catch (IndexOutOfBoundsException a) {
+				sendMsg(message, MESSAGE_NON_RESULT);
 			}
 
 		}
